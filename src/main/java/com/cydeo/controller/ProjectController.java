54b1ac1e419ebb481e04fd12f6ce543edbbba4a7
Hrvoje <hbrkljacic@gmail.com>
2022-12-07 23:12:31 -0500

    apiCreate

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,26 +1,82 @@
 package com.cydeo.controller;
 
 
+import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.ResponseWrapper;
+import com.cydeo.service.ProjectService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/project")
 public class ProjectController {
 
-    public ResponseEntity<ResponseWrapper> getProjects(){}
+    private final ProjectService projectService;
 
-    public ResponseEntity<ResponseWrapper> getProjectByCode(){}
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
 
-    public ResponseEntity<ResponseWrapper> createProject(){}
+    @GetMapping
+    public ResponseEntity<ResponseWrapper> getProjects(){
 
-    public ResponseEntity<ResponseWrapper> updateProject(){}
+        List<ProjectDTO> projectDTOList = projectService.listAllProjects();
 
-    public ResponseEntity<ResponseWrapper> deleteProject(){}
+        return ResponseEntity.ok(new ResponseWrapper("Projects are successfully retrieved",projectDTOList, HttpStatus.OK));
 
-    public ResponseEntity<ResponseWrapper> getProjectByManager(){}
+    }
 
-    public ResponseEntity<ResponseWrapper> managerCompleteProject(){}
+    @GetMapping("/{code}")
+    public ResponseEntity<ResponseWrapper> getProjectByCode(@PathVariable("code") String code){
+
+        ProjectDTO projectDTO = projectService.getByProjectCode(code);
+
+        return ResponseEntity.ok(new ResponseWrapper("Projects are successfully retrieved",projectDTO, HttpStatus.OK));
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseWrapper> createProject(@RequestBody ProjectDTO projectDTO){
+
+        projectService.save(projectDTO);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("Project is successfully created",HttpStatus.CREATED));
+
+    }
+
+    @PutMapping
+    public ResponseEntity<ResponseWrapper> updateProject(@RequestBody ProjectDTO projectDTO){
+
+        projectService.update(projectDTO);
+
+        return ResponseEntity.ok(new ResponseWrapper("Project is successfully updated", HttpStatus.OK));
+
+    }
+
+    @DeleteMapping("/{projectCode}")
+    public ResponseEntity<ResponseWrapper> deleteProject(@PathVariable("projectCode") String projectCode){
+
+        projectService.delete(projectCode);
+
+        return ResponseEntity.ok(new ResponseWrapper("Project is successfully deleted", HttpStatus.OK));
+    }
+
+    @GetMapping("/manager/project-status")
+    public ResponseEntity<ResponseWrapper> getProjectByManager(){
+
+        List<ProjectDTO> projectDTOList = projectService.listAllProjectDetails();
+
+        return ResponseEntity.ok(new ResponseWrapper("Projects are successfully retrieved",projectDTOList, HttpStatus.OK));
+    }
+
+    @PutMapping("manager/complete/{projectCode}")
+    public ResponseEntity<ResponseWrapper> managerCompleteProject(@PathVariable("projectCode") String projectCode){
+
+        projectService.complete(projectCode);
+
+        return ResponseEntity.ok(new ResponseWrapper("Project is successfully completed", HttpStatus.OK));
+
+    }
 }
